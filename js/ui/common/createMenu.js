997286160debb3e07532690f46140693865b7bf7
wanawsha <wanawshaak@gmail.com>
2025-10-17 15:45:30 +0200

    Final polish (Lint/format, e2e env + config working

--- js/ui/common/createMenu.js
+++ js/ui/common/createMenu.js
@@ -8,9 +8,7 @@ export function createMenu() {
 
   const createNavLink = (href, text) => {
     const isActive = isActivePath(href, currentPath) && text !== "Logo";
-    const activeClass = isActive
-      ? "text-blue-300"
-      : "text-white hover:text-blue-200";
+    const activeClass = isActive ? "text-blue-300" : "text-white hover:text-blue-200";
     return `<a href="${href}" class="${activeClass} py-2 px-3 font-medium transition-colors duration-200 ${
       isActive ? "font-bold" : ""
     }">${text}</a>`;
